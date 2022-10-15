27985f3965734a6dcd66efca7125f5f6dc802e64
dan ajayi <56173275+dan-builds-robots@users.noreply.github.com>
2022-10-15 08:58:48 -0400

    Made the footer and header even sexier

--- robotokens-app/Components/Footer.js
+++ robotokens-app/Components/Footer.js
@@ -3,20 +3,96 @@ import styled from 'styled-components'
 
 const Footer = () => {
   return (
-      <Foot>RoboTokens Facebook Twitter Instagram</Foot>
+      <Foot>
+
+        <Container>
+          <Col>
+            <Title>RoboTokens</Title>
+            <Text><Link href="./">TikTok</Link></Text>
+            <Text><Link href="./">Instagram</Link></Text>
+            <Text><Link href="./">Twitter</Link></Text>
+            <Text><Link href="./">Facebook</Link></Text>
+          </Col>
+
+          <Col>
+            <Title>More</Title>
+            <Text><Link href="./">About Us</Link></Text>
+            <Text><Link href="./">Blog</Link></Text>
+          </Col>
+
+          <Col>
+            <Title>Join Us</Title>
+            <Text><Link href="./">Careers</Link></Text>
+            <Text><Link href="./">Make A Suggestion</Link></Text>
+          </Col>
+
+          <Col>
+            <Title>Learn</Title>
+            <Text><Link href="./">Cryptocurrencies</Link></Text>
+            <Text><Link href="./">Blockchain</Link></Text>
+          </Col>
+
+        </Container>
+          
+          <Image2
+            src = "/logo.png"
+            alt="logo"
+            width={50}
+            height={50}
+          />
+          <Text2>© 2022 RoboTokens. All Rights Reserved</Text2>
+      
+      </Foot>
   )
 }
 
 export default Footer
 
 const Foot = styled.div`
-  justify-content:center; 
   box-sizing: border-box;
-  margin: 0px;
   background-color: #B3B3B3;
-  font: impact;
   color: white;
-  display: inline-block;
-  width: 100%;
-  padding: 150px;
+  padding: 120px;
+`
+const Image2 = styled.img`
+  float: bottom;
+`
+
+const Container = styled.div`
+  width: 80%;
+  margin: auto;
+  height: 220px;
+`
+
+const Title = styled.div`
+  color: white;
+  font-weight: 800;
+  font-family: Impact;
+  font-size: 25px;
+  line-height: 200%;
+`
+
+const Col = styled.div`
+  width: 200px;
+  height: 200px;
+  float: left;
+  margin: 10px;
+`
+const Text = styled.div`
+  color: white;
+  font-weight: 300;
+  font-size: 15px;
+  line-height: 200%;
+`
+
+const Text2 = styled.div`
+  color: white;
+  font-weight: 300;
+  font-size: 15px;
+  line-height: 200%;
+  float: right;
+  float: bottom;
+`
+
+const Link = styled.a`
 `
--- robotokens-app/Components/Navbar.js
+++ robotokens-app/Components/Navbar.js
@@ -12,9 +12,17 @@ const Navbar = () => {
 
   return (
     <Header>
+      <BtnHome href="./">
+        <Image2
+          src = "/logo.png"
+          alt="logo"
+          width={50}
+          height={50}
+        />
+      </BtnHome>
       <Title href="./">RoboTokens</Title>
       <Btn1 href = "./wager">Invest</Btn1>
-      <Btn2 href="./findmatch">Battle</Btn2>
+      <Btn2 href="./wager">Battle</Btn2>
       <Btn3 href="./create">Create</Btn3>
     </Header>
   )
@@ -32,6 +40,11 @@ const Header = styled.div`
   padding: 30px;
 `
 
+const BtnHome = styled.a`
+`
+const Image2 = styled.img`
+  margin: 0px 15px 0px 0px;
+`
 
 
 const Title = styled.a`
@@ -52,6 +65,9 @@ const Btn1 = styled.a`
   border: none;
   color: white;
   cursor: pointer;
+  &:hover {
+    background-color: rgba(34, 159, 115, 0.3);
+  }
 `
 
 const Btn2 = styled.a`
@@ -63,9 +79,8 @@ const Btn2 = styled.a`
   padding: 20px;
   color: white;
   border: none;
-  
-  &:hover{
-
+  &:hover {
+    background-color: rgba(190, 11, 41, 0.3);
   }
   
 `
@@ -80,6 +95,9 @@ const Btn3 = styled.a`
   padding: 20px;
   color: white;
   cursor: pointer;
+  &:hover {
+    background-color: rgba(61, 128, 187, 0.3);
+  }
 `
 
 
