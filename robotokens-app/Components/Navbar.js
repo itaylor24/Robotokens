207e6b9239eaaf42488032bfc882c1efd064deeb
Omar R <omarhishamk@gmail.com>
2022-10-15 13:59:55 -0400

    new

--- robotokens-app/Components/Navbar.js
+++ robotokens-app/Components/Navbar.js
@@ -12,8 +12,17 @@ const Navbar = () => {
 
   return (
     <Header>
+      <BtnHome href="./">
+        <Image2
+          src = "/logo.png"
+          alt="logo"
+          width={50}
+          height={50}
+        />
+      </BtnHome>
       <Title href="./">RoboTokens</Title>
-      <Btn1 href = "./wager">Invest</Btn1>
+      <Btn4>Connect Wallet</Btn4>
+      <Btn1 href = "./wager">Wager</Btn1>
       <Btn2 href="./findmatch">Battle</Btn2>
       <Btn3 href="./create">Create</Btn3>
     </Header>
@@ -29,60 +38,99 @@ const Header = styled.div`
   color: white;
   display: inline-block;
   width: 100%;
+  height: 10vw;
   padding: 30px;
 `
-
 
 
 const Title = styled.a`
   display:flex;
   horizontal-padding: 40px;
   margin: 0px;
-  font-size: 50px;
+  font-size: 4vw;
+  color: white;
   display: inline-block;
 `
 
-const Btn1 = styled.a`
-  margin: 0px 0px 0px 10px;
+const Btn1 = styled.button`
+  margin: 0px 0px 0px 1.5vw;
+  font-size: 1.5vw;
+  border-radius: 5px;
   display: inline-block;
   float: right;
   background-color: #229F73;  
   font-family: 'Verdana';
-  padding: 20px;
+  padding: 1.5vw;
   border: none;
   color: white;
   cursor: pointer;
+  &:hover{
+    transform: scale(1.1);
+    border: 3px double black;
+  }
 `
 
-const Btn2 = styled.a`
-  margin: 0px 0px 0px 10px;
+const Btn2 = styled.button`
+  margin: 0px 0px 0px 1.5vw;
   display: inline-block;
+  font-size: 1.5vw;
+  border-radius: 5px;
   float: right;
   background-color: #BE0B29;
   font-family: 'Verdana';
-  padding: 20px;
+  padding: 1.5vw;
   color: white;
   border: none;
-  
+  cursor: pointer;
   &:hover{
-
+    transform: scale(1.1);
+    border: 3px double black;
   }
-  
 `
 
-const Btn3 = styled.a`
-  margin: 0px 0px 0px 10px;
+const Btn3 = styled.button`
+  margin: 0px 0px 0px 1.5vw;
+  font-size: 1.5vw;
+  border-radius: 5px;
   display: inline-block;
   float: right;
   border: none;
   background-color: #3D80BB;
   font-family: 'Verdana';
-  padding: 20px;
+  padding: 1.5vw;
   color: white;
   cursor: pointer;
+  &:hover{
+    transform: scale(1.1);
+    border: 3px double black;
+  }
 `
-
-
+const Btn4 = styled.button`
+  margin: 0px 0px 0px 1.5vw;
+  font-size: 1.5vw;
+  border-radius: 5px;
+  display: inline-block;
+  float: right;
+  border: none;
+  background-color: #cca739;
+  font-family: 'Verdana';
+  padding: 1.5vw;
+  color: white;
+  cursor: pointer;
+  &:hover{
+    transform: scale(1.1);
+    border: 3px double black;
+  }
+`
+const BtnHome = styled.a`
+`
+const Image2 = styled.img`
+  margin: 0px 1.5vw 0px 0px;
+  &:hover{
+    transform: scale(1.1);
+    transition: all 0.5s ease;
+  }
+`
 
 
 export default Navbar
--- robotokens-app/pages/index.js
+++ robotokens-app/pages/index.js
@@ -1,8 +1,59 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Section2 from '../Components/Section2'
+import robot from '../public/robot4.gif'
+import styled from 'styled-components'
 
 export default function Home() {
   return (
-    <div>HELLO</div>
+    <>
+        <Black>
+      <Container>
+        <ImgContainer>
+          <Image
+              src = "/robot4.gif"
+              alt="robot"
+              layout="responsive"
+              width={500}
+              height={300}
+          />
+        </ImgContainer>
+
+        <Text2>
+          Welcome to RoboTokens
+        </Text2>
+
+      </Container>
+    </Black>
+        <Section2 />
+    </>
   )
 }
+
+const Text2 = styled.div`
+  opacity: 1;
+  color: white;
+  font-family: Impact;
+  font-size: 100px;
+  text-align: center;
+  
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`
+
+const ImgContainer = styled.div`
+  backgroundImage: /robot4.gif;
+  opacity: 0.8;
+`
+
+const Container = styled.div`
+  position: relative;
+  text-align: center;
+  color: white;
+`
+
+const Black = styled.div`
+  background-color: black;
+`
