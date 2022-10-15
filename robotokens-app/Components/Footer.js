0fbe4dc87f7abed3b73432545f808a51deeec927
dan ajayi <56173275+dan-builds-robots@users.noreply.github.com>
2022-10-15 05:14:09 -0400

    Add files via upload
    Created a beautiful footer and header

--- robotokens-app/Components/Footer.js
+++ robotokens-app/Components/Footer.js
@@ -3,8 +3,20 @@ import styled from 'styled-components'
 
 const Footer = () => {
   return (
-      <div>Footer</div>
+      <Foot>RoboTokens Facebook Twitter Instagram</Foot>
   )
 }
 
 export default Footer
+
+const Foot = styled.div`
+  justify-content:center; 
+  box-sizing: border-box;
+  margin: 0px;
+  background-color: #B3B3B3;
+  font: impact;
+  color: white;
+  display: inline-block;
+  width: 100%;
+  padding: 150px;
+`
