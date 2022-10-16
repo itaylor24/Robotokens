9c878310039f9922b51cb7eba27a5ec203a6686b
dan ajayi <56173275+dan-builds-robots@users.noreply.github.com>
2022-10-15 21:45:44 -0400

    cleaned up the UI significantly

--- robotokens-app/pages/battle.js
+++ robotokens-app/pages/battle.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import io from 'socket.io-client'; 
 import styled from "styled-components"; 
 import socket from '../utils/socket'; 
+import loading from '../public/loading.gif';
+import Image from 'next/image';
 
 const battle = () => {
     const [action, updateAction] = useState(null); 
@@ -51,20 +53,54 @@ const battle = () => {
     return (
     <div>
       
-         {(!start===true && !roomNo) && <FindMatchButton onClick={handleFindMatch}>
-            Find Match
-        </FindMatchButton>}
-        {(!start===true && roomNo && !gameTime) &&  <WaitDisplay>
-            ROOM: {roomNo} WAITING!!
-          </WaitDisplay>}
-        {(!start===true && roomNo && gameTime) && <MatchWaitDisplay>
-            TIME: {gameTime} BETS: {betAmounts[0]}, {betAmounts[1]}
-          </MatchWaitDisplay>
+      {
+        (!start===true && !roomNo) && 
 
-        }
-        {start === true && <MatchDisplay>
-            MATCH STARTED
-        </MatchDisplay>
+        <FindMatchScreen>
+          <FindMatchButton onClick={handleFindMatch}>Find Match</FindMatchButton>
+        </FindMatchScreen>
+
+      }
+
+      {(!start===true && roomNo && !gameTime) &&  
+        
+        <WaitRoomScreen>
+          <WaitDisplay>You are in room number: {roomNo}</WaitDisplay>
+          <Image
+              src = "/loading.gif"
+              alt="loading"
+              width={200}
+              height={200}
+          />
+        </WaitRoomScreen>
+        
+      }
+
+      {(!start===true && roomNo && gameTime) && 
+      
+        <MatchWaitScreen>
+
+          <TimeContainer>
+            <Text2>Time until battle:</Text2>
+            <Time>{gameTime}</Time>
+          </TimeContainer>
+
+          <BetAmountContainer>
+            <Bet>Pool amount (1st Robot): ${betAmounts[1]}</Bet>
+            <Bet>Pool amount (2nd Robot): ${betAmounts[2]}</Bet>
+          </BetAmountContainer>
+
+        </MatchWaitScreen>
+
+      }
+        {start === true && 
+        
+        <MatchScreen>
+          MATCH STARTED
+          <UnityContainer>
+
+          </UnityContainer>
+        </MatchScreen>
           
         }
 
@@ -73,16 +109,99 @@ const battle = () => {
     )
 }
 
+const MatchScreen = styled.div`
+  align-items: center;
+  justify-content: center;
+  padding: 100px;
+  text-align: center;
+  font-family: Impact;
+  font-size: 4vw;
+`
+
+const BetAmountContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+const UnityContainer = styled.div`
+  padding: 3vw;
+  background-color: #B3B3B3;
+  margin-top: 0.8vw;
+`
+
+const Bet = styled.div`
+  padding: 3vw;
+  background-color: lightgray;
+  color: white:
+  width: fit-content;
+  font-family: Impact;
+  font-size: 2.5vw;
+  margin: 2vw;
+  border-radius: 0.4vw;
+`
+
+const Text2 = styled.div`
+  color: gray;
+  font-size: 1.3vw;
+`
+
+const Time = styled.div`
+  padding: 2vw;
+  background-color: lightgray;
+  color: white:
+  width: fit-content;
+  font-family: Impact;
+  font-size: 3vw;
+  margin-top: 0.8vw;
+  border-radius: 0.4vw;
+`
+
+const FindMatchScreen = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 100px;
+`
+
+const WaitRoomScreen = styled.div`
+  align-items: center;
+  justify-content: center;
+  padding: 100px;
+  text-align: center;
+
+`
+
+const TimeContainer = styled.div`
+  width: fit-content;
+  margin-bottom: 5vw;
+`
+
 const FindMatchButton = styled.button`
-    
+  padding: 5vw;
+  font-size: 2.5vw;
+  font-family: Impact;
+  outline: 0px;
+  border: none;
+  cursor: pointer;
+  background-color: #229F73;
+  color: white;
+  border-radius: 0.4vw;
+  &:hover {
+    background-color: rgba(34, 159, 115, 0.5);
+  }
 `
 const MatchDisplay = styled.div`
   font-size: 30px; 
 `
-const MatchWaitDisplay = styled.div`
-  font-size: 30px; 
+const MatchWaitScreen = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 100px;
+  text-align: center;
+  flex-direction: column;
 `
 const WaitDisplay = styled.div`
-  font-size: 30px; 
+  font-size: 2.5vw;
+  font-family: Impact;
 `
 export default battle
